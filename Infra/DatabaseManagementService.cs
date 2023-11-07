7c268de2d569f7ed2d7cbc834a8aabf975a8b21e
Luan Costa <joseluan620@gmail.com>
2023-11-07 19:18:53 -0300

    add auto-migrate

---
+++ Infra/DatabaseManagementService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra
+{
+    public static class DatabaseManagementService
+    {
+        public static void MigrationInitialization(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var serviceDb = serviceScope.ServiceProvider.GetService<DbContextClass>();
+                serviceDb.Database.Migrate();
+            }
+        }
+    }
+}
