0ee016fdc02320806cdd7b5f325a1234e0eac6e1
Luan Costa <joseluan620@gmail.com>
2023-11-11 18:49:18 -0300

    create home and nav-bar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { ListQueuesComponent } from './list-queues/list-queues.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'queues', component: ListQueuesComponent},
+];
---
+++ frontend/src/app/list-queues/list-queues.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-queues',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-queues.component.html',
+  styleUrl: './list-queues.component.css'
+})
+export class ListQueuesComponent {
+
+}
